0b3227757c251a872df4a8a142c43ba83fc94fe1
Katrina <katrinabyrd88@gmail.com>
2025-01-04 23:41:40 -0500

    Restructured repository to remove LightBnB_WebApp-master subdirectory

--- 1_queries/average_length_of_reservation.sql
+++ 1_queries/average_length_of_reservation.sql
@@ -1,2 +1,8 @@
 SELECT AVG(end_date - start_date) AS average_duration
 FROM reservations;
+
+String.prototype.removeVowels = function () {
+  return this.split("")
+  .filter(char => !"aeiouAEIOU".includes(char))
+  .join("");
+};
